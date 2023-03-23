883f5b30317e85480bc4873bd8eaf8d5d15125e5
My name <you@example.com>
2023-03-23 13:49:40 -0700

    upper and lower

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * _isupper - uppercase letters
+ * isupper - uppercase letters
  * @c: char to check
  *
  * Return: 0 or 1
@@ -9,9 +9,9 @@
 
 int _isupper(int c)
 {
+
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
 }
-
