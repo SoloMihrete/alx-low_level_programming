d2f854ae333260b735c9815cec13dcba463f1f94
My name <you@example.com>
2023-03-24 13:22:20 -0700

    debeging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * positive_or_negative - check for positive or negative numbers
+ *
+ * @i: the number to be checked
+ *
+ * Returne: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else 
+		printf("%d is positive\n",i);
+}
